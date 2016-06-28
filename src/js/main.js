b49adcba73727b6ee8a8d873febb5b1283b02058
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-06-28 14:27:38 -0400

    Got drop down menu functional with basic styling

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,11 @@ import $ from "jquery"
   
    if ( y.type === "text" || y.type === "email" || y.type === "tel")  {
 
-      var textfield = ` <div class="text_field_template">
+      var textfield = `  <form class="forms"> <div class="text_field_template">
+       <i class="fa ${y.icon}"></i>
        <input type="text" name="" class="textfield" placeholder="${y.label}">
 
-      </div>`;
+      </div> </form>` ;
 
       console.log(y.type);
 
@@ -30,19 +31,23 @@ import $ from "jquery"
    else if (y.type === "select"){
    	var optionhtml;  
 
-    y.type.options.forEach(function (n){
+    y.options.forEach(function (n){
     	var options_temp = `<option value="${n.value}">${n.label}</option> `
     	optionhtml += options_temp
-    })
+    console.log(options_temp);
+  console.log(n);
+  })
 
 
-    var select_html = `<div class="text_comment_template">
-       <input type="text" name="" class="commentfield"placeholder="${y.label}">
+    var select_html = `<select class="text_comment_template">
+     
+     <option value="select">Select language...</option>
+     ${optionhtml}
 
-    </div>	`;
 
-    $(".mainform_div").append(comment_html);}
+    </select>	`;
 
+    $(".mainform_div").append(select_html);}
 
 
 
